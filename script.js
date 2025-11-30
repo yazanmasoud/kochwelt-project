5d1487570cf1efbd6fb9eee776b229c9daec546b
Yazan Masoud <yazanmas895@gmail.com>
2025-11-30 20:18:44 +0100

    added contact form to html, function send message added

---
+++ script.js
@@ -0,0 +1,16 @@
+function sendMail(event){﻿
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("https://formspree.io/f/meoyoddr", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./send_mail.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
