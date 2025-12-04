4df6e29f983ca53fe9582c30cec96a7864aa778b
Yazan Masoud <yazanmas895@gmail.com>
2025-12-04 17:50:37 +0100

    added function to calculate the ingredients of recipe to script.js

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function sendMail(event){﻿
         console.log(error);
     });
 }
+function changeAmoutOfIndgredients() {
+       let inputValue = document.getElementById("input-field").value;
+       let amounts = document.querySelectorAll(".amount");
+
+    for (let i = 0; i < amounts.length; i++) {
+        let element = amounts[i];
+        let base = element.getAttribute("data-base");
+        element.innerHTML = base * inputValue;
+    }
+}
