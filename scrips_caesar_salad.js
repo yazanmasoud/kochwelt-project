64f6969ee42d74aada2d66cdbf7e056b5b3b6546
ZahirAfsarDev <zahir.afsar86@gmail.com>
2025-12-08 19:41:34 +0100

    add Caesar_salat +link +html +css

---
+++ scrips_caesar_salad.js
@@ -0,0 +1,11 @@
+document.getElementById("input-field")
+
+
+function ingredientCalculator() {
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
