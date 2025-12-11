3a53343b65214339cf50ad053939a617eb586d84
EldinHasic <eldinlejla287@gmail.com>
2025-12-11 21:12:10 +0100

    started work on jjs loop

---
+++ spaghetti.js
@@ -0,0 +1,8 @@
+function ingredientcalc() {
+    let portionnum = document.getElementById("input").value;
+    let ingredients = document.querySelectorAll(".soloIngredient");
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
