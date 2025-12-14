dc956aa7605134eb767e664050599f1cd56e999d
ChristinaSusHa.christina# <christina.suskahaxhi@gmail.com>
2025-12-14 16:55:14 +0100

    Refactor cupcake recipe page layout and styles; implement ingredient calculation functionality

---
+++ scrips-cupcake.js
@@ -0,0 +1,12 @@
+
+function AmoutOfIndgredients() {
+
+let inputValue = document.getElementById("cupcakeinput-field").value;
+let amounts = document.querySelectorAll(".Portionen");
+
+    for (let i = 0; i < amounts.length; i++) {
+        let element = amounts[i];
+        let base = element.getAttribute("data-bases");
+        element.innerHTML = base * inputValue;
+    }
+}
