60d79086b1be10121a20f2344291f1653821511f
ChristinaSusHa.christina# <christina.suskahaxhi@gmail.com>
2025-12-14 17:20:29 +0100

    Fix ingredient list formatting and clean up JavaScript code

--- scrips-cupcake.js
+++ scrips-cupcake.js
@@ -2,11 +2,11 @@
 function AmoutOfIndgredients() {
 
 let inputValue = document.getElementById("cupcakeinput-field").value;
-let amounts = document.querySelectorAll(".Portionen");
+let amounts = document.querySelectorAll(".Portionen");´
 
     for (let i = 0; i < amounts.length; i++) {
         let element = amounts[i];
         let base = element.getAttribute("data-bases");
-        element.innerHTML = base * inputValue;
+        element.innerHTML = base * inputValue; 
     }
 }
