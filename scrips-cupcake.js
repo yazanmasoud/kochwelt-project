f03b0068ef21e85909a1fc684de6a327778fbd51
ChristinaSusHa.christina# <christina.suskahaxhi@gmail.com>
2025-12-15 18:17:26 +0100

    Fix formatting of ingredient amounts in AmoutOfIndgredients function

--- scrips-cupcake.js
+++ scrips-cupcake.js
@@ -7,8 +7,8 @@ let amounts = document.querySelectorAll(".Portionen");
     for (let i = 0; i < amounts.length; i++) {
         let element = amounts[i];
         let base = element.getAttribute("data-bases");
-        let result = base * inputValue;
-        element.innerHTML = result.toFixed(2);
+        let result = base * inputValue; 
+        element.innerHTML = result.toFixed(2); // Format to 2 decimal places
     
     }
 
